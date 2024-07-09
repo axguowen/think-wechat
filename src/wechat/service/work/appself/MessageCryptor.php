c763a4cb8d97bd01a614a1ca0636744c0806f329
axguowen <axguowen@qq.com>
2024-07-09 13:57:41 +0800

    新增自建应用消息加密解密服务

---
+++ src/wechat/service/work/appself/MessageCryptor.php
@@ -0,0 +1,21 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP Wechat [Simple Wechat Development Kit For ThinkPHP]
+// +----------------------------------------------------------------------
+// | ThinkPHP 微信开发工具包
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: axguowen <axguowen@qq.com>
+// +----------------------------------------------------------------------
+
+namespace think\wechat\service\work\appself;
+
+use think\wechat\service\work\contract\app\MessageCryptor as Service;
+
+/**
+ * 消息加解密服务
+ */
+class MessageCryptor extends Service
+{
+}
